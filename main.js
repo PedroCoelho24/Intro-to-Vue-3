3db536acc3179f2553c6c1ac23131ce657de2539
PedroCoelho24 <pgodinhocoelho@gmail.com>
2024-10-12 19:13:49 +0100

    L2 plus coding challenge

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+//data() is short for data: function()
+//createApp(THIS OBJECT IS CALLED AN OPTIONS OBJECT)
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'this is a pair of socks'
+        }
+    }
+})
+
