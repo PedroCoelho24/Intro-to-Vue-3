41c07d331d4ada162fca52a560d3ecabe6e61f36
PedroCoelho24 <pgodinhocoelho@gmail.com>
2024-10-14 18:20:30 +0100

    L5- end solution

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ],
+              sizes: [
+                { id: 1234, letter: 'S' },
+                { id: 1235, letter: 'M' },
+                { id: 1236, letter: 'L' },
+                { id: 1237, letter: 'XL' },
+                { id: 1238, letter: 'XXL' }
+              ]
         }
     }
 })
